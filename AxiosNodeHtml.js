b447b2d7cb1418588ab4feb8ec542601268d5297
thanteeeee <dandt270948@gmail.com>
2025-02-19 13:45:17 +0700

    upd

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -3,6 +3,7 @@
 
 const express = require('express');
 const axios = require('axios');
+const path = require("path");
 const app = express();
 var bodyParser = require('body-parser');
 
@@ -11,6 +12,7 @@ var bodyParser = require('body-parser');
 const base_url = "http://localhost:5000";
 
 //Set the template engine
+app.set("views",path.join(__dirname,"/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
